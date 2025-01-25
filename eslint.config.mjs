4f0f10735a8d295fbfaead3423cc71376666fad0
Kevin Egstorf <kevin.egstorf@gmail.com>
2025-01-25 11:09:38 +0100

    chore: updated linting config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'constructor-super': 'error',
+      'no-const-assign': 'error',
+      'no-this-before-super': 'error',
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-unused-vars': 'warn',
+      'no-use-before-define': 'error',
+      'valid-typeof': 'error',
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
 ];
